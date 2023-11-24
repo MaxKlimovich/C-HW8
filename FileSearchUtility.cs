14316fb6f626c8771cd7520c91532c368b692a16
Henry Bronx <klimovich.maks@gmail.com>
2023-11-24 11:25:26 +0300

    add HW8

---
+++ FileSearchUtility.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsHwAndSem8;
+
+public class FileSearchUtility
+{
+    public static void SearchForFile(string directory, string fileName)
+        {
+            var files = Directory.EnumerateFiles(directory, fileName, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Искомый файл {fileName} находится по пути: {file}");
+            }
+        }
+
+        public static void SearchForValueInFile(string fileName, string searchValue)
+        {
+            if (File.Exists(fileName))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Contains(searchValue))
+                        {
+                            Console.WriteLine($"Найдено значение {searchValue} в файле {fileName}: {line}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не существует");
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CsHwAndSem8;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string directory = "путь_к_директории"; 
+        string fileName = "искомое_имя_файла"; 
+        string searchValue = "3333";
+
+        FileSearchUtility.SearchForFile(directory, fileName);
+        FileSearchUtility.SearchForValueInFile(fileName, searchValue);
+    }
+}
